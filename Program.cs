f3f4d8e657628801c45deb65d5d0b7300eaa5023
NisreenL <122961483+NisreenL@users.noreply.github.com>
2023-05-19 23:09:03 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter '+' or '-' : ");
+        char operation = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (operation == '+' || operation == '-')
+            {
+                int row = int.Parse(Console.ReadLine());
+                int col = int.Parse(Console.ReadLine());
+
+                double[, ] matrix1 = new double[row, col];
+            
+            Console.WriteLine("Enter the matrix elements : ");
+            
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                while (!double.TryParse(Console.ReadLine(), out matrix1[i, j]))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+                    
+        }
+
+        double[, ] matrix2 = new double[row, col];
+
+        Console.WriteLine("Enter the matrix elements : ");
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                while (!double.TryParse(Console.ReadLine(), out matrix2[i, j]))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+  
+        }
+
+        for (int i = 0; i < row; i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < col; j++)
+            {
+                if (operation == '+'){
+                    Console.Write(matrix1[i, j] + matrix2[i, j] + " ");
+                } else {
+                    Console.Write(matrix1[i, j] - matrix2[i, j] + " ");
+                }
+            }
+        }
+        Console.WriteLine();
+
+    } 
+        
+}
+
+}
+    
+
